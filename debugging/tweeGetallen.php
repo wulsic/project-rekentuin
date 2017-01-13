1c7a5199dd8221bd201b019feb7a3a92be99871f
Winston Jašaragić <wulsic@gmail.com>
2017-01-13 14:16:55 +0100

    tweeGetallen.php: Add divisionOperator function

--- debugging/tweeGetallen.php
+++ debugging/tweeGetallen.php
@@ -14,9 +14,10 @@
 			return $operator[3];
 		}
 		else {
-			return $operator[mt_rand(0,3)];			
+			return $operator[mt_rand(0,3)];
 		}
 	}
+
 	function sommenGenerator($niveau, $rekundigeoperator) {
 		if ($niveau == 4){
 			$max = 10;
@@ -45,32 +46,46 @@
 				$getal2 = $tmpgetal;
 			}
 			$uitkomst = $getal1 - $getal2;
-			$som = "$getal1 - $getal2";			
+			$som = "$getal1 - $getal2";
 		}
 		elseif ($rekundigeoperator == "x") {
 			$uitkomst = $getal1 * $getal2;
 			$som = "$getal1 x $getal2";
 		}
 		else {
-			if ($getal1 == 0){
-				$getal1 = mt_rand($min, $max);
-			}
+			$divisionarray = divisionOperator($getal1, $getal2);
+			print_r($divisionarray);
+			$getal1 = $divisionarray[0];
+			$getal2 = $divisionarray[1];
+			$som = "$getal1 : $getal2";
 			$uitkomst = $getal1 / $getal2;
-				while (!is_int($uitkomst)){
-						$getal1 = mt_rand($min, $max);
-						$getal2 = mt_rand($min, $max);
-						$uitkomst = $getal1 / $getal2;
-
-				}
-						$uitkomst = $getal1 / $getal2;
-				$som = "$getal1 : $getal2";
 		}
 		$somEnuitkomst = array ($som, $uitkomst);
 		return $somEnuitkomst;
 	}
-	
 
-	
+	function divisionOperator ($getal1, $getal2) {
+	if ($getal1 < $getal2){
+		$temp = $getal2; //Wissel getal1 en getal2 van plek
+		$getal2 = $getal1;
+		$getal1 = $temp;
+	}
+//	if ($getal1 % $getal2 !== 0){ //Check of er een heel getal uitkomt
+		$counter = 0;
+		for ($x=1;$x<=$getal1;$x++){
+			$divisioncheck = $getal1 / $x;
+			if (is_int($divisioncheck) == true){ //Check of er een integer uitkomt
+				$divisionarray[$counter] = $divisioncheck;
+				$counter++;
+			}
+		}
+		return array($getal1, $divisionarray[rand(1, $counter -1)]); //Fix offset met counter -1;
+//	}
+	/*else{
+		return array($getal1, $getal2);
+	}*/
+}
+
 ?>
 <pre>
 	<?php
