c51e62955aa980c4ebc64ed4c6a4c111ce6f9da5
Vietchinh Pham <vietchinhpham@hotmail.com>
2017-02-06 17:33:37 +0100

    debug.php: Iterate array session

--- debugging/debug.php
+++ debugging/debug.php
@@ -1,4 +1,38 @@
 <?php
 	session_start();
-	print_r($_SESSION);
+	echo "<br/>
+			<table>";
+				foreach ($_SESSION as $key => $value) {
+					if ($key == "username" || $key == "operator" || $key == "oldOperator" || $key == "group" || $key == "index" || $key == "som" || $key == "uitkomst" || $key == "timestart"){
+					echo "<tr>
+						<td> $key</td>
+						<td> $value</td>";
+					echo "</tr>";	
+					}
+					elseif ($key == "numbers"){
+						echo "<tr>
+							<td> $key</td>";
+						foreach ($value as $key2){
+							echo "<td> $key2</td>";
+						}
+						echo "</tr>";						
+					}
+					elseif ($key == "opdrachtOpslaan"){
+						echo "<tr>
+							<td> $key</td>";
+						echo "</tr>";
+						foreach ($value as $key2 => $value2){
+							echo "<td> $key2</td>";
+							echo "<tr>";
+							foreach ($value2 as $key3 => $value3){
+								echo "<td> $key3</td>";
+								foreach ($value3 as $key4 => $value4){
+									echo "<td> $value4</td>";
+								}
+						echo "</tr>";
+							}
+						}
+					}
+				}
+	echo "</table>";
 ?>
