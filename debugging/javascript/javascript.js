a5ca4b1d24c1789783ecc91118d5d42c75091af4
Vietchinh Pham <vietchinhpham@hotmail.com>
2017-02-07 17:50:57 +0100

    Javascript.js: Test Timer completed.

--- debugging/javascript/javascript.js
+++ debugging/javascript/javascript.js
@@ -1,10 +1,16 @@
 // Section 1 - Variables.
 	// Section 1.1 - Temporary Memory aka a Temporary save storage for one variable.
-var tmpMemory;
+	var tmpMemory;
+	
+	//Section 1.2 - Minutes / Seconds
+	var minutes = 30;
+	var seconds = 0;
 // Section 1 - END
 
 // Section 2 - On DOM (page) ready.
 $(document).ready(function(){
+	// Section 2.1 - Append timer to id="Opdrachten"
+	$("#opdrachten").children("button").after("<p id='timer'> Tijd : " + ("00" + minutes).substr(-2) + ":" + ("00" + seconds).substr(-2) +"</p>");
 	// Section 2.1 - Fade in
 	$("#startpagina").fadeIn("slow").css("display", "inline-flex");
 	// Section 2.2 - Form submit
@@ -39,21 +45,43 @@ $(document).ready(function(){
 });
 // Section 2 - END
 
-// Section 3 - Function previous. Fade out from current page to fade in the previous page from the hierarchy. Refer to html page for the hierarchy.
-
-//Function - pageVisibility checks whether the 1 given argument page is visible or not
+// Section 3 - pageVisibility checks whether the 1 given argument page is visible or not
 function pageVisibility(id1){
 	var text = $(id1).css("display") != "none";
 	return text;
 }
 
-//Function - fadeAnimation manages the transition between the 2 given arguments
+// Section 4 - fadeAnimation manages the transition between the 2 given arguments
 function fadeAnimation(id1, id2){
 	$(id1).fadeOut("slow", function(){
 		$(id2).fadeIn("slow");
 	});
 }
+function createTable(id1, table){
+	$(id1).fadeOut("slow", function(){
+		$("#uitslag").children("p").remove();
+		$("#uitslag").children("table").remove();
+		ifEresults = (table == "eResults") ? $("#uitslag").append("<p> ???????? wat </p>") : $("#uitslag").append(table);
+		$("#uitslag").fadeIn("slow");
+	});
+	timeLimit(); // Minutes and Seconds are 0 so it is going to reset the minutes to a set variable;
+}
+// Section 5 - username verfications.
+function usernameVerify(txt) {
+    if (txt.value == '') {
+        txt.setCustomValidity('Vul je naam in');
+    }
+    else if(txt.validity.patternMismatch){
+        txt.setCustomValidity('Gebruik alleen letters');
+    }
+    else {
+        txt.setCustomValidity('');
+    }
+    return true;
+}
+// Section 5 - END
 
+// Section 6 - Function previous. Fade out from current page to fade in the previous page from the hierarchy. Refer to the index page for the hierarchy.
 function previous(){
 	if (pageVisibility("#operators")){
 		fadeAnimation("#operators", "#groepen");
@@ -69,24 +97,33 @@ function previous(){
 		fadeAnimation("#uitslag", "#opdrachtenSelectie")
 	}
 }
-// Section 3 - END
+// Section 6 - END
 
-// Section 4 - username verfications.
-function usernameVerify(txt) {
-    if (txt.value == '') {
-        txt.setCustomValidity('Vul je naam in');
-    }
-    else if(txt.validity.patternMismatch){
-        txt.setCustomValidity('Gebruik alleen letters');
-    }
-    else {
-        txt.setCustomValidity('');
-    }
-    return true;
+// Section 7 - Time limit for the user making a test.
+function timeLimit() {
+	if (minutes != 0 || seconds != 0){
+		seconds--;
+		if (seconds <= 0 && minutes != 0){
+			seconds = 60;
+			minutes--
+		}
+		if (minutes == 0 && seconds == 0){
+			clearTimeout(timeLimitloop);
+			post(null);
+		}
+		else {
+			timeLimitloop = setTimeout(timeLimit, 1000);	
+		}
+		$("#timer").text("Tijd : " + ("00" + minutes).substr(-2) + ":" + ("00" + seconds).substr(-2));		
+	}
+	else {
+		minutes = 30;
+		timeLimit();
+	}
 }
-// Section 4 - END
+// Section 7 - END
 
-// Section 5 - Popup function modal()
+// Section 6 - Popup function modal()
 function modal(id, som, uitkomst, antwoord, foutofGoed, naam){
 	var text = "";
 	var modal = ("#" + id + "modal");
@@ -129,13 +166,15 @@ function modal(id, som, uitkomst, antwoord, foutofGoed, naam){
 			whenNotremove = ($("#startpagina").css("display") != "none") ? null : $(modal).children(".modal-content").children("p").remove();				
 			var somEnuitkomst = som + " = " + uitkomst;
 		});
+		$("input[name='input']").val(null).focus();
 	});
 }
-// Section 5 - END
+// Section 6 - END
 
-// Section 6 - Submit form replacement. POST using AJAX.
+// Section 7 - Submit form replacement. POST using AJAX.
 function post(val) {
-	var success = "";  // Reset success va 	riable
+	
+	var success = "";  // Reset success variable
 	var dataType = ""; // Reset dataType variable
 	var dataSend = ""; // Reset dataSend variable
 	
@@ -165,142 +204,138 @@ function post(val) {
 		anything with e infront of it is short version for empty. Like: eResults ; emptyResults
 		
 	}*/
-	
-	if (pageVisibility("#startpagina")){
-		$("input[name='input'], input[type='submit']").prop('disabled', true);
+	if (minutes == 0 && seconds == 0){
 		dataSend = {
-			functions: "callLoginsystem",
-			username: val
+			functions: "callResultpage"
 		}
-		dataType = "text";
 		success = function success(data){
-			$("#startpagina").fadeOut("slow", function(){
-				$("#groepen").children("h1:first-child").text("hallo " + data);
-				$("#groepen").fadeIn("slow").css("display", "inline-flex");
-			});
+				createTable("#opdrachten", data);
 		}
 	}
-	if (pageVisibility("#groepen")){
-		dataSend = {
-			functions: "group", 
-			group: val.replace(/[^0-9]/g, "")
-		}
-		success = function success(){
-			fadeAnimation("#groepen", "#operators");
-		}
-	}
-	if (pageVisibility("#operators")){
-		$("input[name='input'], input[type='submit']").prop('disabled', false);
-		if (val == ":"){
-			var val = val.replace(":", "/");
-		}
-		else if (val == "x"){
-			var val = val.replace("x", "*");
-		}
-		dataSend = {
-			functions: (val == "Ja") ? "delete" : "callRekundigeoperator",
-			operator: (val == "Ja") ? null : val
-		}
-		dataType = (val == "Toets" || val == "Ja") ? "JSON" : "text";
-		success = function success(data){
-			if (data == "true") {
-				modal("testAlreadymade");
+	else {
+		
+		if (pageVisibility("#startpagina")){
+			$("input[name='input'], input[type='submit']").prop('disabled', true);
+			dataSend = {
+				functions: "callLoginsystem",
+				username: val
 			}
-			else {
-				$("#operators").fadeOut("slow", function(){
-					if (val == "Toets" || val == "Ja"){
-						if (val == "Ja") {
-							tmpMemory = "Toets";
-						}
-						$("#opdrachten").children("form").children("h1").text(data.replace(/\"/g, ""));
-						$("#opdrachten").fadeIn("slow").css("display", "inline-flex");
-						$("input[name='input']").val(null).focus();
-					}
-					else if (val == "Resultaten"){
+			dataType = "text";
+			success = function success(data){
+				$("#startpagina").fadeOut("slow", function(){
+					$("#groepen").children("h1:first-child").text("hallo " + data);
+					$("#groepen").fadeIn("slow").css("display", "inline-flex");
+				});
+			}
+		}
+		if (pageVisibility("#groepen")){
+			dataSend = {
+				functions: "group", 
+				group: val.replace(/[^0-9]/g, "")
+			}
+			success = function success(){
+				fadeAnimation("#groepen", "#operators");
+			}
+		}
+		if (pageVisibility("#operators")){
+			$("input[name='input'], input[type='submit']").prop('disabled', false);
+			if (val == ":"){
+				var val = val.replace(":", "/");
+			}
+			else if (val == "x"){
+				var val = val.replace("x", "*");
+			}
+			dataSend = {
+				functions: (val == "Ja") ? "delete" : "callRekundigeoperator",
+				operator: (val == "Ja") ? null : val
+			}
+			dataType = (val == "Toets" || val == "Ja") ? "JSON" : "text";
+			success = function success(data){
+				if (data == "true") {
+					modal("testAlreadymade");
+				}
+				else {
+					if (val == "Resultaten"){
 						tmpMemory = "Toets";
-						$("#uitslag").children("table").remove();
-						$("#uitslag").append(data);
-						$("#uitslag").fadeIn("slow");
+						createTable("#operators", data);
 					}
 					else {
-						$("#opdrachtenSelectie").fadeIn("slow").css("display", "inline-flex");
+						$("#operators").fadeOut("slow", function(){
+							if (val == "Toets" || val == "Ja"){
+								$("#timer").css("display", "inline-block");
+								if (val == "Ja") {
+									tmpMemory = "Toets";
+								}
+								$("#opdrachten").children("form").children("h1").text(data.replace(/\"/g, ""));
+								$("#opdrachten").fadeIn("slow",  function(){timeLimit();}).css("display", "inline-flex");
+								$("input[name='input']").val(null).focus();
+							}
+							else {
+								$("#opdrachtenSelectie").fadeIn("slow").css("display", "inline-flex");
+							}
+						});	
+					}			
+				}
+			}
+		}
+		if (pageVisibility("#opdrachtenSelectie")){
+			if (val == "Resultaten"){
+				dataSend = {
+					functions: "callResultpage"
+				}
+				success = function success(data){
+					if (data == "eResults"){
+						modal("eResults");
 					}
-				});				
-			}
-		}
-	}
-	if (pageVisibility("#opdrachtenSelectie")){
-		if (val == "Resultaten"){
-			dataSend = {
-				functions: "callResultpage"
-			}
-			success = function success(data){
-				if (data == "eResults"){
-					modal("eResults");
-				}
-				else {
-					$("#opdrachtenSelectie").fadeOut("slow", function(){
-						$("#uitslag").children("table").remove();
-						$("#uitslag").append(data);
-						$("#uitslag").fadeIn("slow");
-					});	
+					else {
+						createTable("#opdrachtenSelectie", data);
+					}
 				}
 			}
+			else {
+				dataSend = {
+					functions: (val == "Ja" || val == "Opnieuw beginnen") ? "delete" : "callindexCheckerandGenerator",
+					index: (val == "Toets") ? null : (val == "Ja") ? tmpMemory : val
+				}
+				dataType = "JSON";
+				success = (val == "Opnieuw beginnen") ? null : function success(data){
+					if (data[0] == "true") {
+						modal("alreadyMade", data[1][0], data[1][2], data[1][3]);
+					}
+					else {
+						ifTimerexist = (pageVisibility("#timer")) ? $("#timer").css("display", "none") : null;
+						$("#opdrachtenSelectie").fadeOut("slow", function(){
+							$("#opdrachten").children("form").children("h1").text(data.replace(/\"/g, ""));
+							$("#opdrachten").fadeIn("slow").css("display", "inline-flex");
+							$("input[name='input']").val(null).focus();
+						});
+					}
+				}
+			}
 		}
-		else {
+		if (pageVisibility("#opdrachten")){
 			dataSend = {
-				functions: (val == "Ja" || val == "Opnieuw beginnen") ? "delete" : "callindexCheckerandGenerator",
-				index: (val == "Toets") ? null : (val == "Ja") ? tmpMemory : val
+				functions: "callControlsaveAndassignmentGenerator",
+				antwoord: val
 			}
 			dataType = "JSON";
-			success = (val == "Opnieuw beginnen") ? null : function success(data){
-				if (data[0] == "true") {
-					modal("alreadyMade", data[1][0], data[1][2], data[1][3]);
+			success = function success(data){
+				$("input[name='input'], input[type='submit']").prop('disabled', true);
+				if (data[0] == "eNumber"){
+					createTable("#opdrachten", data[1]);
 				}
 				else {
-					$("#opdrachtenSelectie").fadeOut("slow", function(){
-						$("#opdrachten").children("form").children("h1").text(data.replace(/\"/g, ""));
-						$("#opdrachten").fadeIn("slow").css("display", "inline-flex");
-						$("input[name='input']").val(null).focus();
+					ifToets = (tmpMemory == "Toets") ? null : modal("response", data[0],data[1], data[2], data[3], data[4]) ;
+					$('input[name="input"]').val(null);
+					$("#opdrachten").children("form").children("h1").fadeOut("fast", function(){
+						ifToets2 = (tmpMemory == "Toets") ? $("input[name='input'], input[type='submit']").prop('disabled', false) : null;
+						ifToets4 = (tmpMemory == "Toets") ? $("input[name='input']").val(null).focus() : null;
+						$("#opdrachten").children("form").children("h1").text(ifToets = (tmpMemory == "Toets") ? data : data[5]).fadeIn("fast");
 					});
 				}
 			}
-		}
-	}
-	if (pageVisibility("#opdrachten")){
-		dataSend = {
-			functions: "callControlsaveAndassignmentGenerator",
-			antwoord: val
-		}
-		dataType = "JSON";
-		success = function success(data){
-			$("input[name='input'], input[type='submit']").prop('disabled', true);
-			if (data[0] == "eNumber"){
-				$("#opdrachten").fadeOut("slow", function(){
-					$("#uitslag").children("table").remove();
-					$("#uitslag").append(data[1]);
-					$("#uitslag").fadeIn("slow");
-				});
-			}
-			else {
-				ifToets = (tmpMemory == "Toets") ? null : modal("response", data[0],data[1], data[2], data[3], data[4]) ;
-				$('input[name="input"]').val(null);
-				$("#opdrachten").children("form").children("h1").fadeOut("fast", function(){
-					ifToets2 = (tmpMemory == "Toets") ? $("input[name='input'], input[type='submit']").prop('disabled', false) : null;
-					ifToets4 = (tmpMemory == "Toets") ? $("input[name='input']").val(null).focus() : null;
-					$("#opdrachten").children("form").children("h1").text(ifToets = (tmpMemory == "Toets") ? data : data[5]).fadeIn("fast");
-				});
-			}
-		}
-	}
-	if ($("#uitslag").css("display") != "none"){
-		dataSend = {
-			functions: "results",
-		}
-		success = function success(data){
-			$("#uitslag").append(data);				
-		}
-	}
+		}	
+	}	
 	$.ajax({
 	   type: "POST",
 	   url: "ajax.php", // The url where the post is going to be send and the response orginate.
